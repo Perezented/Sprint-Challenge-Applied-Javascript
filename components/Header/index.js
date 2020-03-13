2cdf3a4c2baaac4ea00d8a8800da983fae7976e5
Perezented <57721734+Perezented@users.noreply.github.com>
2020-03-13 10:36:37 -0500

    Finished the header part.

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,28 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+//getting where I want to append all this now
+const attachHeaderHere = document.querySelector(".header-container");
+
+function Header() {
+    //creating elements
+    const header = document.createElement("div"),
+        date = document.createElement("span"),
+        heading1 = document.createElement("h1"),
+        temp = document.createElement("span");
+    //adding classes
+    header.classList.add("header");
+    date.classList.add("date");
+    temp.classList.add("temp");
+    //textContent
+    date.textContent = "SMARCH 28, 2019";
+    heading1.textContent = "Lambda Times";
+    temp.textContent = "98°";
+    //glueing them together
+    header.append(date, heading1, temp);
+    //attach to document
+    attachHeaderHere.append(header);
+    console.log(header);
+    return Header;
+}
+Header();
