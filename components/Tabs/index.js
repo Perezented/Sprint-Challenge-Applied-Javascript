0d0accae142607b2ccad99605620bc3f19839165
Perezented <57721734+Perezented@users.noreply.github.com>
2020-03-13 10:54:03 -0500

    Tabs are somewhat done. I dont think its supposed to be how I have it though.

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,26 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+// const title = document.querySelector("title");
+
+axios
+    .get("https://lambda-times-backend.herokuapp.com/topics")
+    .then(response => {
+        console.log(response);
+        console.log(response.data.topics);
+        const techData = response.data.topics;
+        techData.forEach(value => {
+            const attachTabsHere = document.querySelector(".tabs");
+            const tab = document.createElement("div");
+            console.log(value);
+            tab.classList.add("tabs");
+            tab.textContent = value;
+            console.log(tab);
+            attachTabsHere.append(tab);
+            // document.querySelector(".tabs").style.display = "grid";
+            title.style.width = "100%";
+        });
+    })
+    .catch(error => {
+        console.log(ERRRRRRRRRPPPOPKTKRRRRRRRSSSSSSS, error);
+    });
